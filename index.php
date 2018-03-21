794b5c8ec6a5629f94f3f6668f95e1b7a6bbcdc3
Rowantrek <Rowan@nrgnetwork.org>
2018-03-21 19:01:49 +1100

    Added DSD To Index.php
    Added DSD To Index.php
    Fixed Responsiveness for DSD / Index.php

--- index.php
+++ index.php
@@ -11,8 +11,9 @@
 		exit;
 	}
 	?>
+	<!-- Content Wrapper -->
 	<div class="container">
-
+	<iframe class="gdoc" width="800" height="800" src="https://docs.google.com/document/d/e/2PACX-1vR5qbbL20kNEZGkLcauvuUQXASBYOZnzIviLixNjYs45Gg2r2nkz2buzYfJm1400TK0w7WEVGERgk_U/pub?embedded=true"></iframe>
 	</div>
 
 </body>
